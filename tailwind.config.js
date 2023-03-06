a382befc4c9a8ffbf6650c86df7ccb5dc07c0674
sandajama <sandajama29@gmail.com>
2023-03-06 01:56:14 -0800

    2

---
+++ tailwind.config.js
@@ -0,0 +1,55 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#E22727',
+        secondary: '#380B0B',
+        testry: '#512E2E',
+        collorlo:'#1E1E1E;',
+        background: '#B5B5B5',
+      
+        properties: {
+          height: '60px',
+          width: '450px',
+          left: '167px',
+          top: '577px',
+          border:' 15px',
+          padding: '10px',
+          height: '1080px',
+          width: '791px',
+          
+
+
+          position: absolute,
+             left: '8.7%',
+             right: '67.86%',
+             top: '53.43%',
+             bottom: '41.02%', 
+             
+             fontfamily: 'Nunito',
+             fontsize: '18px',
+             fontstyle: 'italic',
+             fontweight: '300',
+             lineheight: '25px',
+             letterspacing: '0em',
+             textalign: 'left',
+
+          
+      
+
+
+
+      },
+      
+
+
+        
+
+
+      },
+    },
+  },
+  plugins: [],
+}
